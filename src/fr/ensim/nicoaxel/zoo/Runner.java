08e00c7fb80f2f42eec1b1bd943df6538ca59079
Axel M. (amacz13) <axel.maczkowiak@live.fr>
2018-12-17 20:26:00 +0100

    Revert "BugFix & Engine Improvements"
    This reverts commit 1c7cacb2f8d0f65f9f170db2b42d48bead58afbf.

---
+++ src/fr/ensim/nicoaxel/zoo/Runner.java
@@ -0,0 +1,32 @@
+package fr.ensim.nicoaxel.zoo;
+
+import fr.ensim.nicoaxel.zoo.types.ObjectType;
+import javafx.scene.canvas.GraphicsContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Runner implements Runnable{
+
+    private static final Logger log = LogManager.getRootLogger();
+
+    private int c = 0;
+    private GraphicsContext gc;
+    private Zoo zoo;
+
+    public Runner(GraphicsContext g, Zoo z) {
+        zoo = z;
+        gc = g;
+    }
+
+
+    @Override
+    public void run() {
+        log.info("Temps "+(c++)+" ("+Main.zoo.nbAnimal()+" animals)");
+
+        try {
+            zoo.action(gc);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
