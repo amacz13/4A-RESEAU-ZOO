59aa1ee1df4fa954c39ef2d4f6cbc70df1c1bbbd
Nicolas <nack714@hotmail.fr>
2018-12-17 16:48:47 +0100

    Server

---
+++ src/fr/ensim/nicoaxel/server/Service.java
@@ -0,0 +1,39 @@
+package fr.ensim.nicoaxel.server;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+class Service implements Runnable {
+    private Socket maSocket;
+
+    Service(Socket s) {
+        maSocket = s;
+    }
+
+    public void run() {
+        try {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(maSocket.getInputStream()));
+            String qui = bf.readLine();
+            System.out.println(qui + " : vient de se connecter");
+            Thread.sleep(500);
+
+            String msg = "";
+            PrintWriter pw;
+            do{
+
+                pw = new PrintWriter(new OutputStreamWriter(maSocket.getOutputStream()));
+                pw.println(qui + " : "+ msg);
+            }while (!msg.equals("STOP"));
+            pw.close();
+            bf.close();
+            maSocket.close();
+        } catch (Exception e) {
+            System.err.println("Erreur : " + e);
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
