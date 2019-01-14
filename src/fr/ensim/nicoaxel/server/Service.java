cbb088a6029d508e1c449d7e100b7ddb94525cff
Nicolas <nack714@hotmail.fr>
2019-01-14 18:00:40 +0100

    Serveur good

--- src/fr/ensim/nicoaxel/server/Service.java
+++ src/fr/ensim/nicoaxel/server/Service.java
@@ -5,6 +5,7 @@ import fr.ensim.nicoaxel.server.animals.Elephant;
 import fr.ensim.nicoaxel.server.animals.Fox;
 import fr.ensim.nicoaxel.server.animals.Lion;
 import fr.ensim.nicoaxel.server.animals.Zebra;
+import fr.ensim.nicoaxel.server.types.Espece;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -49,40 +50,92 @@ class Service implements Runnable {
             Main.addListAnimals(ua);
 
             do{
-                System.out.println("#DEBUT TOUR");
                 msg = bf.readLine();
-                System.out.println("Received : "+msg);
-                if(msg.equals("STOP")) {
+                //System.out.println("Received : "+msg);
+
+                if(msg.equals("STOP")){
+                    //System.out.println("ARRET !");
+                    break;
+                }
+                if (msg.equals("STARTANIMALS")) {
+                    System.out.println(qui + " : #DEBUT TOUR");
+                    ua.clear();
+                }
+                if(msg.startsWith("[Animal]")){
+                    System.out.println(qui + " : Animal : "+msg.split(" ")[1]);
+                    Animal a = new Lion();
+                    switch (msg.split(" ")[1]) {
+                        case "LION":
+                            a = new Lion();
+                            a.setEspece(Espece.LION);
+                            break;
+                        case "ZEBRA":
+                            a = new Zebra();
+                            a.setEspece(Espece.ZEBRA);
+                            break;
+                        case "ELEPHANT":
+                            a = new Elephant();
+                            a.setEspece(Espece.ELEPHANT);
+                            break;
+                        case "FOX":
+                            a = new Fox();
+                            a.setEspece(Espece.FOX);
+                            break;
+                    }
+                    a.setX(Integer.parseInt(msg.split(" ")[2]));
+                    a.setY(Integer.parseInt(msg.split(" ")[3]));
+                    ua.addAnimal(a);
+                }
+
+                if(msg.equals("STOPANIMALS")){
+                    pw.println("STARTANIMALSOTHER");
+                    pw.flush();
+                    for (Animal a : Main.getlistAnimals(qui)) {
+                        pw.println(a.toSend());
+                        pw.flush();
+                    }
+                    pw.println("ENDANIMALSOTHER");
+                    pw.flush();
+                    System.out.println(qui +" :  #FIN TOUR");
+
+                }
+/*
+                if(!msg.equals("STOP")) {
                     if (msg.equals("STARTANIMALS")) {
                         ua.clear();
-                    }
-                    while (!msg.equals("STOPANIMALS")) {
-                        Animal a = new Lion();
-                        switch (msg.split(" ")[1]) {
-                            case "LION":
-                                a = new Lion();
-                                break;
-                            case "ZEBRA":
-                                a = new Zebra();
-                                break;
-                            case "ELEPHANT":
-                                a = new Elephant();
-                                break;
-                            case "FOX":
-                                a = new Fox();
-                                break;
+                    }else{
+                        while (!msg.equals("STOPANIMALS")) {
+                            Animal a = new Lion();
+                            switch (msg.split(" ")[1]) {
+                                case "LION":
+                                    a = new Lion();
+                                    break;
+                                case "ZEBRA":
+                                    a = new Zebra();
+                                    break;
+                                case "ELEPHANT":
+                                    a = new Elephant();
+                                    break;
+                                case "FOX":
+                                    a = new Fox();
+                                    break;
+                            }
+                            a.setX(Integer.parseInt(msg.split(" ")[2]));
+                            a.setY(Integer.parseInt(msg.split(" ")[3]));
+                            ua.addAnimal(a);
+                            msg = bf.readLine();
+
                         }
-                        a.setX(Integer.parseInt(msg.split(" ")[2]));
-                        a.setY(Integer.parseInt(msg.split(" ")[3]));
-                        ua.addAnimal(a);
                     }
                     for (Animal a : Main.getlistAnimals(qui)) {
                         pw.println(a.toSend());
                     }
                 }
                 System.out.println("#FIN TOUR");
-
+*/
             }while (!msg.equals("STOP"));
+
+
             System.out.println(qui+" est partit manger");
             pw.close();
             bf.close();
